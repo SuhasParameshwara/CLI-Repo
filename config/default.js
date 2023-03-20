56493117a82373947081740e445e6e0a90e284b7
Suhas Parameshwara <suhas.parameshwara@kovai.co>
2023-03-20 20:05:56 +0530

    updated the default js url

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   // eslint-disable-next-line global-require
   cli: require('../package.json').name,
   env: {
-    eu_baseUrl: "https://localhost:44310",
+    eu_baseUrl: "https://apihub.dev.document360.net",
     us_baseUrl: "https://apihub.us.document360.io"
   }
 };
