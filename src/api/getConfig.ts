0524a918d17e26fe18f4b6b69158892d270db548
suhas-parameshwara <suhas.parameshwara@kovai.co>
2023-04-19 14:21:19 +0530

    fix: bump version

--- src/api/getConfig.ts
+++ src/api/getConfig.ts
@@ -15,6 +15,8 @@ export default function getConfig(): EnvironmentConfig {
 }
 
 
+
+
 export function getApiHubUrl(): string {
   return process.env.D360_APIHUB_URL || configStore.get('apihubUrl');
 }
